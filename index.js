f7143599de9f7fa0ff1bf9c3aee0fd200b345fd7
mac- <mac.ang311@gmail.com>
2018-05-17 11:24:51 -0600

    make browser compatible version

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 const changeCase = require('change-case')
 
 const AVAILABLE_METHODS = [
-    'camelCase',
-    'constantCase',
-    'dotCase',
-    'headerCase',
-    'lowerCase',
-    'lowerCaseFirst',
-    'noCase',
-    'paramCase',
-    'pascalCase',
-    'pathCase',
-    'sentenceCase',
-    'snakeCase',
-    'swapCase',
-    'titleCase',
-    'upperCase',
-    'upperCaseFirst',
+  'camelCase',
+  'constantCase',
+  'dotCase',
+  'headerCase',
+  'lowerCase',
+  'lowerCaseFirst',
+  'noCase',
+  'paramCase',
+  'pascalCase',
+  'pathCase',
+  'sentenceCase',
+  'snakeCase',
+  'swapCase',
+  'titleCase',
+  'upperCase',
+  'upperCaseFirst',
 ]
 
 const changeCasePostProcessor = {
-    getAvailableMethods: () => AVAILABLE_METHODS.slice(0),
-    changeCase: {
-        name: 'changeCase',
-        type: 'postProcessor',
-        process: (value, key, { method } = {}) => {
-            if (!method || !AVAILABLE_METHODS.includes(method)) {
-                return value
-            }
-            return changeCase[method](value)
-        }
-    }
+  getAvailableMethods: () => AVAILABLE_METHODS.slice(0),
+  changeCase: {
+    name: 'changeCase',
+    type: 'postProcessor',
+    process: (value, key, { method } = {}) => {
+      if (!method || !AVAILABLE_METHODS.includes(method)) {
+        return value
+      }
+      return changeCase[method](value)
+    },
+  },
 }
 
 AVAILABLE_METHODS.forEach((method) => {
-    changeCasePostProcessor[method] = {
-        name: method,
-        type: 'postProcessor',
-        process: (value) => changeCase[method](value)
-    }
+  changeCasePostProcessor[method] = {
+    name: method,
+    type: 'postProcessor',
+    process: (value) => changeCase[method](value),
+  }
 })
 
 module.exports = changeCasePostProcessor
